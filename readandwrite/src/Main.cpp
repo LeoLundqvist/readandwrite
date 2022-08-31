de8241b61d77305d06b4ccb249e67e7d14a65a92
LeoLundqvist <leolundqvist05@gmail.com>
2022-08-31 14:08:34 +0200

    Added namespace Lib
    template print function

--- readandwrite/src/Main.cpp
+++ readandwrite/src/Main.cpp
@@ -1,3 +1,19 @@
-int main() {
+#include <iostream>
+
+namespace Lib 
+{
+	template <typename T>
+	void Print(T t)
+	{
+		std::cout << t << "\n";
+	}
+
+}
+
+
+
+int main() 
+{
+	Lib::Print(123);
 
 }
